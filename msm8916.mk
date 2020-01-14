64c3468a04240db6a5fe186c0f3cc53eab40a925
DarkDroidDev <darkdroid.dev@gmail.com>
2020-01-14 22:18:54 +0100

    msm8916-common: Go back to rild.libpath

--- msm8916.mk
+++ msm8916.mk
@@ -432,7 +432,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.rild.nitz_short_ons_3="" \
     ril.subscription.types=NV,RUIM \
     DEVICE_PROVISIONED=1 \
-    vendor.rild.libpath=/system/lib/libsec-ril.so \
+    rild.libpath=/system/lib/libsec-ril.so \
     ro.multisim.set_audio_params=true
 
 # SAMP SPCM
