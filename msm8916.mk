0366d16cfa7ae3e52d559fb23f13261935dbbf33
Binay Tiwari <tbinay40@gmail.com>
2020-01-14 12:53:19 +0545

    msm8916-common: Remove deprecated ipv6 tethering support

--- msm8916.mk
+++ msm8916.mk
@@ -124,12 +124,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
-# Ebtables
-PRODUCT_PACKAGES += \
-    ebtables \
-    ethertypes \
-    libebtc
-
 # Encryption
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.lineage
