25979218b7ca2643ff9ff749a79d74e74835cf1b
DarkDroidDev <darkdroid.dev@gmail.com>
2020-01-15 16:44:21 +0100

    msm8916-common: Also build service for HAL

--- msm8916.mk
+++ msm8916.mk
@@ -170,6 +170,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
     gralloc.msm8916 \
     hwcomposer.msm8916 \
     libgenlock \
@@ -184,7 +185,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Ebtables
 PRODUCT_PACKAGES += \
@@ -228,6 +230,7 @@ PRODUCT_COPY_FILES += \
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
     com.android.location.provider \
     com.android.location.provider.xml \
     gps.msm8916 \
