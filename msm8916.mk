003ac67aafd7371674269781790570c0962a65dc
mhkjahromi <m.h.k.jahromi@gmail.com>
2020-03-24 10:55:46 +0100

    msm8916-common: add SamsungParts
    Change-Id: I40d5cd31004bd30f4125ee8cfa5f17b313d73411

--- msm8916.mk
+++ msm8916.mk
@@ -242,6 +242,10 @@ PRODUCT_PACKAGES += \
     libkeyutils \
     tcpdump
 
+# Parts
+PRODUCT_PACKAGES += \
+    SamsungParts
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
